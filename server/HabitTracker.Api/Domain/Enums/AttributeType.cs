2d7f42c0b05124353110616194caf26923e7768f
fredmikhail <67843788+fredmikhail@users.noreply.github.com>
2026-07-20 02:10:34 -0400

    Define habit categories and attribute types

---
+++ server/HabitTracker.Api/Domain/Enums/AttributeType.cs
@@ -0,0 +1,13 @@
+namespace HabitTracker.Api.Domain.Enums;
+
+public enum AttributeType
+{
+    Discipline = 1,
+    Fitness = 2,
+    Vitality = 3,
+    Focus = 4,
+    Mind = 5,
+    Resilience = 6,
+    Social = 7,
+    Purpose = 8
+}
---
+++ server/HabitTracker.Api/Domain/Enums/HabitCategory.cs
@@ -0,0 +1,17 @@
+namespace HabitTracker.Api.Domain.Enums;
+
+public enum HabitCategory
+{
+    FitnessAndMovement = 1,
+    HealthAndRecovery = 2,
+    LearningAndSkills = 3,
+    WorkAndCareer = 4,
+    DailyLifeAndOrganization = 5,
+    MoneyAndFinance = 6,
+    RelationshipsAndCommunity = 7,
+    EmotionalWellBeing = 8,
+    SpiritualityAndPurpose = 9,
+    CreativityAndRecreation = 10,
+    SelfControlAndBoundaries = 11,
+    GeneralGrowth = 12
+}
